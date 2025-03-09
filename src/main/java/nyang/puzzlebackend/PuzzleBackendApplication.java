ce6372f1300650373a0241f0a323225db1f43bd1
김민식 <kmss69052@naver.com>
2025-03-09 16:44:13 +0900

    init project

---
+++ src/main/java/nyang/puzzlebackend/PuzzleBackendApplication.java
@@ -0,0 +1,13 @@
+package nyang.puzzlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PuzzleBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/nyang/puzzlebackend/PuzzleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nyang.puzzlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

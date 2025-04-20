b68a69a284f35b222254bf32c377f1cb3cea66f0
김민식 <kmss69052@naver.com>
2025-04-20 20:05:54 +0900

    feature: ErrorContents 를 만드는 정적 팩토리 메서드 추가

--- src/main/java/nyang/puzzlebackend/api/common/ApiResponse.java
+++ src/main/java/nyang/puzzlebackend/api/common/ApiResponse.java
@@ -6,21 +6,27 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.time.Instant;
 import nyang.puzzlebackend.global.error.ErrorContent;
+import nyang.puzzlebackend.global.error.ErrorContents;
 
 @JsonInclude(value = Include.NON_NULL)
 public record ApiResponse<T>(
     String result,
     T data,
     String timestamp,
-    ErrorContent error
+    ErrorContent error,
+    ErrorContents errors
 ) {
 
   private ApiResponse(String result, T data) {
-    this(result, data, ISO_INSTANT.format(Instant.now()), null);
+    this(result, data, ISO_INSTANT.format(Instant.now()), null, null);
   }
 
   private ApiResponse(String result, ErrorContent error) {
-    this(result, null, ISO_INSTANT.format(Instant.now()), error);
+    this(result, null, ISO_INSTANT.format(Instant.now()), error, null);
+  }
+
+  private ApiResponse(String result, ErrorContents errors) {
+    this(result, null, ISO_INSTANT.format(Instant.now()), null, errors);
   }
 
   public static <T> ApiResponse<T> ok(T data) {
@@ -28,6 +34,10 @@ public record ApiResponse<T>(
   }
 
   public static <T> ApiResponse<T> error(ErrorContent errorContent) {
+    return new ApiResponse<>("error", errorContent);
+  }
+
+  public static <T> ApiResponse<T> errors(ErrorContents errorContent) {
     return new ApiResponse<>("error", errorContent);
   }
 }

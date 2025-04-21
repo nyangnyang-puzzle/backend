00925da669b3e3e3a20d6073f4282004465dab20
김민식 <kmss69052@naver.com>
2025-04-21 11:06:17 +0900

    feat: 닉네임 등록 Service, Controller 추가 - `ApiResponse.java`: data 값이 굳이 필요 없는 경우에 사용하는 ok() 정적 팩터리 메서드 - `UserRepository.java`: 삭제되지 않은 유저(`isDeleted` = false)만 가져올 수 있도록 `findById()` 재정의

--- src/main/java/nyang/puzzlebackend/api/common/ApiResponse.java
+++ src/main/java/nyang/puzzlebackend/api/common/ApiResponse.java
@@ -2,13 +2,10 @@ package nyang.puzzlebackend.api.common;
 
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.time.Instant;
 import nyang.puzzlebackend.global.error.ErrorContent;
 import nyang.puzzlebackend.global.error.ErrorContents;
 
-@JsonInclude(value = Include.NON_NULL)
 public record ApiResponse<T>(
     String result,
     T data,
@@ -31,6 +28,10 @@ public record ApiResponse<T>(
 
   public static <T> ApiResponse<T> ok(T data) {
     return new ApiResponse<>("success", data);
+  }
+
+  public static <T> ApiResponse<T> ok() {
+    return new ApiResponse<>("success", null, ISO_INSTANT.format(Instant.now()), null, null);
   }
 
   public static <T> ApiResponse<T> error(ErrorContent errorContent) {

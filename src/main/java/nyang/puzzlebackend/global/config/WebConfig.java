ab45f1f829178cee71cf8e1e125fdf2abc79063a
김민식 <kmss69052@naver.com>
2025-04-21 10:57:19 +0900

    feat: 인증 Interceptor, 인증 관련 ArgumentResolver Config 등록

--- src/main/java/nyang/puzzlebackend/global/config/WebConfig.java
+++ src/main/java/nyang/puzzlebackend/global/config/WebConfig.java
@@ -3,12 +3,45 @@ package nyang.puzzlebackend.global.config;
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import nyang.puzzlebackend.auth.AuthPrincipalArgumentResolver;
+import nyang.puzzlebackend.auth.AuthenticationInterceptor;
+import nyang.puzzlebackend.auth.OptionalAuthPrincipalArgumentResolver;
 import nyang.puzzlebackend.global.logging.LoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+  private final AuthenticationInterceptor authenticationInterceptor;
+  private final OptionalAuthPrincipalArgumentResolver optionalAuthPrincipalArgumentResolver;
+  private final AuthPrincipalArgumentResolver authPrincipalArgumentResolver;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(authenticationInterceptor)
+        .order(1)
+        .excludePathPatterns(
+            // nickname
+            "/user/nickname/available-check/**",
+
+            // auth
+            "/auth/oauth2/**"
+        );
+
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(authPrincipalArgumentResolver);
+    resolvers.add(optionalAuthPrincipalArgumentResolver);
+  }
 
   @Bean
   public ObjectMapper objectMapper() {

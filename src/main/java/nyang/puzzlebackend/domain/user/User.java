595938280b378e84a4f6121e67ec95c56d28af2e
김민식 <kmss69052@naver.com>
2025-04-21 11:01:18 +0900

    feat: User 엔티티 타입(record -> class) 및 생성자 메서드 변경

--- src/main/java/nyang/puzzlebackend/domain/user/User.java
+++ src/main/java/nyang/puzzlebackend/domain/user/User.java
@@ -1,7 +1,10 @@
 package nyang.puzzlebackend.domain.user;
 
 import java.time.LocalDateTime;
+import lombok.Getter;
 import nyang.puzzlebackend.auth.oauth.OAuthProvider;
+import nyang.puzzlebackend.global.error.ErrorCode;
+import nyang.puzzlebackend.global.error.PuzzleException;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -9,21 +12,46 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.Field.Write;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
+@Getter
 @Document(collection = "users")
-public record User(
-    @Id ObjectId id,
-    @Indexed(unique = true) String oauthId,
-    @Field(targetType = FieldType.STRING) OAuthProvider oAuthProvider,
-    @Indexed(unique = true) String nickname,
-    @CreatedDate LocalDateTime createdAt,
-    @LastModifiedDate LocalDateTime updatedAt,
-    LocalDateTime deletedAt,
-    boolean isDeleted
-) {
+public class User {
+
+  @Id
+  private ObjectId id;
+
+  @Indexed(unique = true)
+  private String oauthId;
+
+  @Field(targetType = FieldType.STRING)
+  private OAuthProvider oAuthProvider;
+
+  @Indexed(unique = true)
+  private String nickname;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+  private LocalDateTime deletedAt;
+
+  @Field(write = Write.NON_NULL)
+  private boolean isDeleted;
 
   public User(String oauthId, OAuthProvider oAuthProvider) {
-    this(null, oauthId, oAuthProvider, null, null, null, null, false);
+    this.oauthId = oauthId;
+    this.oAuthProvider = oAuthProvider;
+    this.isDeleted = false;
+  }
+
+  public void initNickname(String nickname) {
+    if (this.nickname != null)
+      throw new PuzzleException(ErrorCode.U005);
+    this.nickname = nickname;
   }
 }
+

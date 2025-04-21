e68aab0531bc48748e64020528a7246b27270346
김민식 <kmss69052@naver.com>
2025-04-21 12:49:58 +0900

    bugfix: Resolves #12

--- src/main/java/nyang/puzzlebackend/api/common/ApiResponse.java
+++ src/main/java/nyang/puzzlebackend/api/common/ApiResponse.java
@@ -2,10 +2,13 @@ package nyang.puzzlebackend.api.common;
 
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.time.Instant;
 import nyang.puzzlebackend.global.error.ErrorContent;
 import nyang.puzzlebackend.global.error.ErrorContents;
 
+@JsonInclude(value = Include.NON_NULL)
 public record ApiResponse<T>(
     String result,
     T data,

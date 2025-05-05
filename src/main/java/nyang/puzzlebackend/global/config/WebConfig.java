5dee26ee40ba134a6d0b5dc9fdac2be86b478cd9
김민식 <kmss69052@naver.com>
2025-05-05 14:02:35 +0900

    feat: upload 용 presigned url 생성하는 api 경로 인증 인터셉터 예외 처리

--- src/main/java/nyang/puzzlebackend/global/config/WebConfig.java
+++ src/main/java/nyang/puzzlebackend/global/config/WebConfig.java
@@ -27,7 +27,10 @@ public class WebConfig implements WebMvcConfigurer {
             "/user/nickname/available-check/**",
 
             // auth
-            "/auth/oauth2/**"
+            "/auth/oauth2/**",
+
+            // s3 presigned url
+            "/api/presigned/upload"
         );
 
   }
